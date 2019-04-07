722bbf6bc9dbc3b01cc7da5b85d1e8a1d0d7552f
LoneyDay <1428148434@qq.com>
2019-04-07 20:02:53 +0800

    电商全端第三天2019-04-06

---
+++ public/m/js/productlist.js
@@ -0,0 +1,173 @@
+$(function (){
+
+    var search;
+
+    searchProduct();
+    nowSearchProduct();
+    sortProduct();
+    pullProduct();
+
+    //接受参数并搜索的函数
+    function searchProduct(){
+
+        search = getQueryString('search');
+
+        queryProduct({proName: search,page: 1,pageSize: 2});
+
+    }
+
+    //定义当前按钮搜索的函数
+    function nowSearchProduct(){
+
+        $('.btn-search').on('tap',function(){
+            search = $('.input-search').val().trim();
+
+            if(search == null){
+                return;
+            }
+
+            queryProduct({proName: search,page: 1,pageSize: 2});
+        });
+
+    }
+
+    //公共请求api
+    function queryProduct(data){
+        data.page =  data.page || 1,
+        data.pageSize = data.pageSize || 2,
+        $.ajax({
+            url: '/product/queryProduct',
+            data: data,
+            success: function(res){
+                //console.log(res);
+                var html = template('productlistTpl',res);
+                //console.log(html);
+                
+                $('.product-list .mui-row').html(html);
+            }
+        });
+    }
+
+    //商品排序
+    function sortProduct(){
+
+        //1给所有那啥添加点击事件
+        $('.product-list .mui-card-header a').on('tap',function(){
+            
+            //2.获取当前排序类型 data-type
+            var type = $(this).data('type');
+            //console.log(type);
+
+            //3.获取当前排序顺序 data-sort
+            var sort = $(this).data('sort');
+            //console.log(sort);
+
+            //4.修改data-sort的值
+            if(sort == 1){
+                sort = 2;
+                $(this).find('i').removeClass('fa-angle-down').addClass('fa-angle-up');
+            }else{
+                sort = 1;
+                $(this).find('i').removeClass('fa-angle-up').addClass('fa-angle-down');
+            }
+
+            //5.修改完了重新给当前元素设置这个属性值
+            $(this).data('sort',sort);
+            //console.log(sort);
+
+            //6.当前点击的那啥腰间盘突出
+            $(this).addClass('active').siblings().removeClass('active');
+            
+            //7.这就是调用公共函数的参数了
+            var obj = {
+                proName: search,
+                page: 1,
+                pageSize: 2
+            }
+
+            //8.给上面的obj动态添加一个属性，直接在上面写会出问题的啊
+            obj[type] = sort;
+            //console.log(obj);
+
+            //9.调用函数就ok了
+            queryProduct(obj);
+
+        });
+    }
+
+    //下拉刷新和下拉加载更多
+    function pullProduct(){
+        mui.init({
+            pullRefresh: {
+                container: '#pullrefresh',
+                down: {
+                    contentdown: "下拉刷新",
+                    contentover: "松手即可刷新",
+                    contentrefresh: "拼命加载中",
+                    callback: pulldownRefresh
+                },
+                up: {
+                    contentrefresh: '正在加载...',
+                    callback: pullupRefresh
+                }
+            }
+        });
+        /**
+         * 下拉刷新具体业务实现
+         */
+        function pulldownRefresh() {
+            setTimeout(() => {
+                queryProduct({
+                    page: 1,
+                    pageSize: 3,
+                    proName: search
+                });
+    
+                mui('#pullrefresh').pullRefresh().endPulldownToRefresh(); //refresh completed
+            }, 1500);
+        }
+        var page = 1;
+        /**
+         * 上拉加载具体业务实现
+         */
+        function pullupRefresh() {
+            setTimeout(() => {
+                page++;
+
+                $.ajax({
+                    url: '/product/queryProduct',
+                    data: {
+                        page: page,
+                        pageSize: 2,
+                        proName: search
+                    },
+                    success: function(res){
+                        if(res.data.length > 0){
+                            //console.log(res);
+                            var html = template('productlistTpl',res);
+                            //console.log(html);
+                            
+                            $('.product-list .mui-row').append(html);
+
+                            mui('#pullrefresh').pullRefresh().endPullupToRefresh(); //参数为true代表没有更多数据了。
+                        }else{
+                            mui('#pullrefresh').pullRefresh().endPullupToRefresh(true); //参数为true代表没有更多数据了。
+                        }
+                    }
+                });
+            }, 1500);
+        }
+    }
+
+
+    // 使用正则匹配url参数 返回这个匹配成功的值 根据参数名获取参数的值
+    function getQueryString(name) {
+        var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) {
+            // 别人之前使用unescape 方式解密  但是我们默认是encodeURI加密 使用 decodeURI 解密
+            return decodeURI(r[2]);
+        }
+        return null;
+    }
+});
--- public/m/js/search.js
+++ public/m/js/search.js
@@ -1,6 +1,8 @@
 $(function(){
     addHistory();
     queryHistory();
+    deleteHistory();
+    clearHistory();
 
     // 初始化搜索历史区域滚动
     mui('.mui-scroll-wrapper').scroll({
@@ -16,13 +18,7 @@ $(function(){
                 return;
             }
 
-            var arr = localStorage.getItem('searchHistory');
-
-            if(arr == null){
-                arr = [];
-            }else{
-                arr = JSON.parse(arr);
-            }
+            var arr = getHistoryData();
 
             for(var i=0;i<arr.length;i++){
                 if(arr[i] == search){
@@ -32,25 +28,66 @@ $(function(){
             }
             arr.unshift(search);
             
-            localStorage.setItem('searchHistory',JSON.stringify(arr));
+            setHistoryData(arr);
 
             $('.input-search').val('');
             queryHistory();
+
+            location = './productlist.html?search=' + search;
         });
     }
 
     //查询搜索历史
     function queryHistory(){
-        var arr = localStorage.getItem('searchHistory');
-        // console.log(arr);
         
-        if(arr == null){
-            arr=[];
-        }else{
-            arr = JSON.parse(arr);
-        }
+        var arr = getHistoryData();
         
         var html = template('searchHistoryTpl',{rows: arr});
         $('.search-history ul').html(html);
+    }
+
+    //删除搜索历史
+    function deleteHistory(){
+        $('.mui-card-content ul').on('tap','.mui-badge',function(){
+
+            var index = $(this).data('index');
+            
+            var arr =  getHistoryData();
+            
+            arr.splice(index,1);
+            
+            setHistoryData(arr);
+            queryHistory();
+        });
+    }
+
+    //清空搜索历史
+    function clearHistory(){
+        //推荐removeItem，因为clear清空所有的值（别的数据别的页面不能乱删啊）
+        $('.btn-clear').on('tap',function(){
+            
+            localStorage.removeItem('searchHistory');
+            queryHistory();
+
+        });
+    }
+
+    //定义封装函数，查询历史记录的数据
+    function getHistoryData(){
+
+        var arr = localStorage.getItem('searchHistory');
+            
+        if(arr == null){
+            arr = [];
+        }else{
+            arr = JSON.parse(arr);
+        }
+
+        return arr;
+    }
+
+    //定义封装函数，设置balabala
+    function setHistoryData(arr){
+        localStorage.setItem('searchHistory',JSON.stringify(arr));
     }
 });
